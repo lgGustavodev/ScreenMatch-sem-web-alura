f5347e829367b3b3c7fc2ae5637a4205228349e8
Luis Gustavo Paz Barbosa <138590403+lgGustavodev@users.noreply.github.com>
2025-06-05 16:04:26 -0300

    inicio do projeto spring

---
+++ src/main/java/br/com/alura/Screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,30 @@
+package br.com.alura.Screenmatch;
+//ApiKey = 3b627f3f
+import br.com.alura.Screenmatch.model.DadosSerie;
+import br.com.alura.Screenmatch.service.ConsumoApi;
+import br.com.alura.Screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados(
+				"https://www.omdbapi.com/?t=Supernatural&apikey=3b627f3f");
+//		System.out.println(json);
+//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+
+	}
+}
