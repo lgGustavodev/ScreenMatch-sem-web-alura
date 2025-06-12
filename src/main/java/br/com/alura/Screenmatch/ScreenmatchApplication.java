d93f50e59c04e640e5bc5838f3324d1409eb1bec
Luis Gustavo Paz Barbosa <138590403+lgGustavodev@users.noreply.github.com>
2025-06-12 20:07:33 -0300

    deu certo

--- src/main/java/br/com/alura/Screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/Screenmatch/ScreenmatchApplication.java
@@ -3,6 +3,7 @@ package br.com.alura.Screenmatch;
 import br.com.alura.Screenmatch.model.DadosEpisodio;
 import br.com.alura.Screenmatch.model.DadosSerie;
 import br.com.alura.Screenmatch.model.DadosTemporada;
+import br.com.alura.Screenmatch.principal.Principal;
 import br.com.alura.Screenmatch.service.ConsumoApi;
 import br.com.alura.Screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
@@ -21,29 +22,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados(
-				"https://www.omdbapi.com/?t=Supernatural&apikey=3b627f3f");
-//		System.out.println(json);
-//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-		json = consumoApi.obterDados("https://www.omdbapi.com/?t=Supernatural&season=1&episode=2&apikey=3b627f3f");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json,DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
-
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-
-		for (int i = 1; i<= dados.totalTemporadas(); i++){
-			json = consumoApi.obterDados("https://www.omdbapi.com/?t=Supernatural&season="+ i +"&apikey=3b627f3f");
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-
-
-		}
-		temporadas.forEach(System.out::println);
+		Principal principal = new Principal();
+		principal.exibieMenu();
 	}
 }
--- src/main/java/br/com/alura/Screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/Screenmatch/principal/Principal.java
@@ -1,13 +1,20 @@
 package br.com.alura.Screenmatch.principal;
 
+import br.com.alura.Screenmatch.model.DadosSerie;
+import br.com.alura.Screenmatch.model.DadosTemporada;
 import br.com.alura.Screenmatch.service.ConsumoApi;
+import br.com.alura.Screenmatch.service.ConverteDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
 
     private ConsumoApi consumo = new ConsumoApi();
+
+    private ConverteDados conversor = new ConverteDados();
 
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
 
@@ -16,12 +23,21 @@ public class Principal {
     public void exibieMenu(){
         System.out.println("Digite o nome da Serio para Buscar os dados:");
         var nomeSerie = leitura.nextLine();
-
         var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
-
-
-
-
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
         //"https://www.omdbapi.com/?t=Supernatural&apikey=3b627f3f"
+
+        System.out.println(dados);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for (int i = 1; i<= dados.totalTemporadas(); i++){
+            json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+
+
+        }
+        temporadas.forEach(System.out::println);
     }
 }
