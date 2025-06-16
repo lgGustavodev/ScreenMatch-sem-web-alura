a47db061b888dce2a601efbd9871e8424df1e677
Luis Gustavo Paz Barbosa <138590403+lgGustavodev@users.noreply.github.com>
2025-06-16 12:24:38 -0300

    Update Principal.java

--- src/main/java/br/com/alura/Screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/Screenmatch/principal/Principal.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
@@ -42,17 +43,31 @@ public class Principal {
         }
         temporadas.forEach(System.out::println);
 
-//        for (int i = 0; i < dados.totalTemporadas(); i++){
-//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-//            for (int j = 0; j < episodiosTemporada.size(); j++){
-//                System.out.println(episodiosTemporada.get(j).titulo());
-//            }
-//        }
-
+        for (int i = 0; i < dados.totalTemporadas(); i++){
+            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+            for (int j = 0; j < episodiosTemporada.size(); j++){
+                System.out.println(episodiosTemporada.get(j).titulo());
+            }
+        }
 
         temporadas.forEach(t -> t.episodios().forEach(e-> System.out.println(e.titulo())));
 
-        List<String> nomes = Arrays.asList("Jacque", "Iasmin", "Paulo", "Nico");
-        nomes.stream().sorted().forEach(System.out::println);
+//        List<String> nomes = Arrays.asList("Jacque", "Iasmin", "Paulo", "Nico");
+//        nomes.stream()
+//                .sorted().limit(3)
+//                .filter(n -> n.startsWith("N"))
+//                .map(n -> n.toUpperCase())
+//                .forEach(System.out::println);
+//
+//
+//        List<String> palavras = Arrays.asList("UVA", "JAVA", "JAVASCRIPT", "PYthon");
+//        List<Integer> tamanhos = palavras.stream().map(s -> s.length()).collect(Collectors.toList());
+//        System.out.println(tamanhos);
+//
+//        List<String> nomes2 = Arrays.asList("GUSTAVO", "SAVIO", "BEATRIZ");
+//        nomes2.stream().forEach(nome -> System.out.println("Ola, " + nome + "!"));
+
+
     }
+
 }
