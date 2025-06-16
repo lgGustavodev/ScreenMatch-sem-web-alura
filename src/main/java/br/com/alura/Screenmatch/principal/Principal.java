e85bf2a18b88e2b9027a1f7c1b15a6eace601968
Luis Gustavo Paz Barbosa <138590403+lgGustavodev@users.noreply.github.com>
2025-06-16 14:32:41 -0300

    Update Principal.java

--- src/main/java/br/com/alura/Screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/Screenmatch/principal/Principal.java
@@ -6,10 +6,7 @@ import br.com.alura.Screenmatch.model.DadosTemporada;
 import br.com.alura.Screenmatch.service.ConsumoApi;
 import br.com.alura.Screenmatch.service.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Principal {
@@ -43,12 +40,12 @@ public class Principal {
         }
         temporadas.forEach(System.out::println);
 
-        for (int i = 0; i < dados.totalTemporadas(); i++){
-            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
-            for (int j = 0; j < episodiosTemporada.size(); j++){
-                System.out.println(episodiosTemporada.get(j).titulo());
-            }
-        }
+//        for (int i = 0; i < dados.totalTemporadas(); i++){
+//            List<DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+//            for (int j = 0; j < episodiosTemporada.size(); j++){
+//                System.out.println(episodiosTemporada.get(j).titulo());
+//            }
+//        }
 
         temporadas.forEach(t -> t.episodios().forEach(e-> System.out.println(e.titulo())));
 
@@ -67,7 +64,15 @@ public class Principal {
 //        List<String> nomes2 = Arrays.asList("GUSTAVO", "SAVIO", "BEATRIZ");
 //        nomes2.stream().forEach(nome -> System.out.println("Ola, " + nome + "!"));
 
-
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+        System.out.println("\nTop 5 Episodios:");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
     }
 
 }
